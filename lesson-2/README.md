f8d14a9a924fb54ddf2b5f78d08cddd1993306e8
Daniel Fernandes Gonçalves de Oliveira <danielfeenandes@gmail.com>
2019-05-28 17:19:10 -0300

    Added lesson 2

---
+++ lesson-2/README.md
@@ -0,0 +1,10 @@
+# Mouse Selection/Hover System
+
+This system uses bounding boxes to creat a skirt that shows wich object is being hovered or if it is selected. 
+
+![Object with skirt arround](https://github.com/Carnaux/Mouse-Hover-Selection/tree/master/imgs/selectionGif.gif)
+
+## Check out the demo!!
+
+https://carnaux.github.io/Mouse-Hover-Selection/
+"# Laparoscopic-Trainer" 
---
+++ lesson-2/j1.js
@@ -0,0 +1,55 @@
+var objects = [];
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color("rgb(120,120,135)")
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+let renderDiv = document.getElementById("renderDiv");
+renderDiv.appendChild( renderer.domElement );
+
+var orbit = new THREE.OrbitControls( camera, renderDiv);
+orbit.update();
+orbit.addEventListener( 'change', render );
+
+var axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+let lap = new Laparoscopic(scene);
+
+lap.init();
+
+var geometry = new THREE.DodecahedronGeometry(1);
+var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+var rock = new THREE.Mesh( geometry, material );
+
+lap.createTarget("rock", 3, "touch");
+
+camera.position.z = 30;
+camera.position.y = 20;
+camera.lookAt(0,0,0);
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+animate();
+
+function animate() {
+    requestAnimationFrame( animate );
+    lap.update();
+
+    render();
+}
+
+function render(){
+    renderer.render( scene, camera );
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}// resize window
+
+
+
